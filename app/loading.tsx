c72050f17a37426f6050bb5312afa441f3a3ec8a
SmartPunt <evo3332111@gmail.com>
2026-07-07 10:56:44 +0800

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,11 +3,11 @@ export default function Loading() {
     <main className="flex min-h-screen items-center justify-center bg-black px-6">
       <div className="flex flex-col items-center gap-5 text-center">
 <div className="flex h-[85vw] w-[85vw] max-h-[520px] max-w-[520px] items-center justify-center overflow-hidden bg-black">
-          <img
-            src="/smartpunt-icon-512.png"
-            alt="SmartPunt"
-className="h-full w-full object-contain"
-          />
+<img
+  src="/smartpunt-icon-512.png"
+  alt="SmartPunt"
+  className="h-full w-full bg-black object-contain"
+/>
         </div>
 
         <div>
