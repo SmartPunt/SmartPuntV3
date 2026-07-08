e13727b1104536efe44409b4898ed5f265056920
SmartPunt <evo3332111@gmail.com>
2026-07-08 09:08:37 +0800

    Update page.tsx

--- app/subscriber-dashboard/page.tsx
+++ app/subscriber-dashboard/page.tsx
@@ -141,6 +141,8 @@ export default async function SubscriberDashboardPage() {
         initialActiveUserBetCount={activeUserBetCount}
         initialPublishedRaces={livePicksData.currentRaces}
         initialPublishedRunners={livePicksData.currentRunners}
+        initialScoringRaces={livePicksData.races}
+        initialScoringRunners={livePicksData.runners}
         initialHorses={livePicksData.horses}
         initialMeetings={livePicksData.currentMeetings}
         initialJockeyProfiles={livePicksData.jockeyProfiles}
