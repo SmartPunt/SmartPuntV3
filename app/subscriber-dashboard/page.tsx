7adce78701b9897a42cb541e005db0c2ed1261f1
SmartPunt <evo3332111@gmail.com>
2026-07-08 08:51:50 +0800

    Update page.tsx

--- app/subscriber-dashboard/page.tsx
+++ app/subscriber-dashboard/page.tsx
@@ -143,6 +143,7 @@ export default async function SubscriberDashboardPage() {
         initialPublishedRunners={livePicksData.currentRunners}
         initialHorses={livePicksData.horses}
         initialMeetings={livePicksData.currentMeetings}
+        initialJockeyProfiles={livePicksData.jockeyProfiles}
         initialResultedUserBets={resultedUserBetsQuery.data ?? []}
       />
     </AppEntryLoader>
