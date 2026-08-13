8c7d77873e8225066c819f4446db067d8970ea32
SmartPunt <evo3332111@gmail.com>
2026-08-13 11:22:33 +0800

    Update loading.tsx

--- app/the-vault/loading.tsx
+++ app/the-vault/loading.tsx
@@ -3,11 +3,11 @@ import VaultLoadingVisual from "@/components/vault-loading-visual";
 export default function TheVaultLoading() {
   return (
     <div className="fixed inset-0 z-[9999] overflow-hidden bg-black">
-      <VaultLoadingVisual
-        progress={null}
-        animateWheel={false}
-        finishFlash={false}
-      />
+<VaultLoadingVisual
+  progress={null}
+  animateWheel={false}
+  blackout={false}
+/>
     </div>
   );
 }
